14eeee6bdb90e8359976a6cf9a176fa2b0409c9a
ahmedGHANIM327 <ahmedghanim902@gmail.com>
2022-06-12 19:16:29 +0200

    Contact Page

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../General/Navbar/Navbar'
+import './Blog.css'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import PostBloc from '../General/Posts/PostBloc';
+import Pagination from '@mui/material/Pagination';
+import Footer from '../General/Footer/Footer';
 
 export default function Blog() {
+
+  const [categorie , setCategorie] = useState('ALL');
+  const handleCategories = (e) => {
+    setCategorie(e.target.value);
+  }
+
+  const [page, setPage] = useState(1);
+  const handlePages = (event, value) => {
+    setPage(value);
+  };
+
   return (
     <div id="Blog">
         <div id="navbar">
             <Navbar />
+        </div>
+        <div id='blog-head' className="section bloc-text">
+          <div className="bloc-text">
+          <h1>Welcome to <span className='blog-span'>The world of Tech</span></h1>
+          <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.Quisque velit nisi, 
+              pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.Quisque velit nisi, 
+              pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.</p>
+          </div>
+        </div>
+
+        <div className="blog-posts section">
+          <div className="top">
+            <h2>All Posts</h2>
+            <div className="right">
+            <FormControl className='form-select'>
+              <InputLabel id="select-label-categorie">Categories</InputLabel>
+              <Select
+                labelId="select-label-categorie"
+                id="select-label-categorie"
+                value={categorie}
+                label="Categories"
+                onChange={handleCategories}
+              >
+                <MenuItem value={'all'}>All</MenuItem>
+                <MenuItem value={'mobile'}>Web Dev</MenuItem>
+                <MenuItem value={'web'}>Mobile Dev</MenuItem>
+              </Select>
+            </FormControl>
+            </div>
+          </div>
+          <div className="posts">
+            <PostBloc />
+            <PostBloc />
+            <PostBloc />
+            <PostBloc />
+            <PostBloc />
+          </div>
+          <div className="pagination">
+            <Pagination count={10} page={page} onChange={handlePages}/>
+          </div>
+        </div>
+
+        <div id="footer">
+            <Footer />
         </div>
     </div>
   )
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from '../General/Navbar/Navbar'
+import Categories from '../General/Categories/Categories'
+import Footer from '../General/Footer/Footer';
+import './Contact.css'
+import { Grid } from '@mui/material';
 
 export default function Contact() {
   return (
@@ -7,6 +11,46 @@ export default function Contact() {
         <div id="navbar">
             <Navbar />
         </div>
+
+        <div id='blog-head' className="section bloc-text">
+          <div className="bloc-text">
+          <h1>Contact <span className='blog-span'>US</span></h1>
+          <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.Quisque velit nisi, 
+              pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.Quisque velit nisi, 
+              pretium ut lacinia in, elementum id enim. 
+              Vestibulum ante ipsum primis in faucibus. Quisque velit nisi,
+              pretium ut lacinia in, elementum id enim.</p>
+          </div>
+        </div>
+
+        <div className="section">
+              <Grid container className='bloc_contact' spacing={2}>
+                <Grid className='bloc_left' item md={4}>
+                  <ul>
+                    <li>+33674311624</li>
+                    <li>contact@ahmed-ghanim.com</li>
+                    <li>Toulouse , France</li>
+                  </ul>
+                </Grid>
+                <Grid className='bloc_right' item md={8}>
+
+                </Grid>
+              </Grid>
+        </div>
+
+        <div id="categories" className="section">
+            <h2>Categories</h2>
+            <Categories />
+        </div>
+
+        <div id="footer">
+            <Footer />
+        </div>
+
     </div>
   )
 }
